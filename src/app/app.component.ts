bf382e91b0f06662213e43179e159878625754b5
lanhvv <vuvanlanh10@gmail.com>
2024-03-24 09:58:41 +0700

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CloudinaryService } from './service/cloudinary.service';
+import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,11 @@ import { CloudinaryService } from './service/cloudinary.service';
 export class AppComponent implements OnInit{
   title = 'manage_book_angular';
 
-  constructor(private cloudinary: CloudinaryService) {
-  }
+  cld:CloudinaryImage;
+    constructor(private cloudinary: CloudinaryService) {
+        const cld = new Cloudinary({cloud: {cloudName: 'dwuhxymqb'}});
+        this.cld = cld.image('cld-sample-5');
+    }
   ngOnInit(): void {
     this.cloudinary.upload('https://cdn.pixabay.com/photo/2016/05/05/02/37/sunset-1373171_1280.jpg').subscribe(resp => {
       debugger
